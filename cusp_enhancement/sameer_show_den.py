6ce512de744901cb6107a3e2679f19fab563a36d
Prasoon Vishwakarma <prasoonv@derecho6.hsn.de.hpc.ucar.edu>
2025-07-12 17:00:37 -0600

    fixing errors in files

--- cusp_enhancement/sameer_show_den.py
+++ cusp_enhancement/sameer_show_den.py
@@ -27,11 +27,11 @@ time_ref=dt.datetime(2012,3,21)
 
 
 path = '/glade/work/prasoonv/gitm/'
-output_type = '2dall' # '2dall' or '3dall'
+output_type = '3dall' # '2dall' or '3dall'
 by_nature = ['by_neg_10/', 'by_pos_10/']
 precip = ['no_precip/', 'e_precip/']
 by = by_nature[0]
-prec = precip[1]
+prec = precip[0]
 
 data_fn = ''
 if output_type == '2dall':
@@ -45,13 +45,14 @@ if output_type == '3dall':
 
 
 data=np.load(data_fn)                                                           
+print('Data keys in zpf file:', list(data.keys()))
 ut=data['ut']                                                                   
 lon=data['glon']                                                                
 lat=data['glat']                                                                
-rho=data['Rho']*1e12                                                                                   
-pot=data['pot']/1000  
-int_jh=data['int_jh']*1000
-#print(int_jh)
+rho=data['Rho']*1e12
+if output_type == '2dall':                                                                                   
+    pot=data['pot']/1000  
+    int_jh=data['int_jh']*1000
 
 save_pt=data_pt + 'sum_plot/'
 if not os.path.exists(save_pt):
@@ -65,19 +66,23 @@ cmap=make_mid(256)
 Change the figure size and parameters in the subplots_adjust
 """
 fig,axes=plt.subplots(4,6,figsize=(56,60),subplot_kw={'projection': 'polar'})
-plt.subplots_adjust(left=0.08,right=0.92,bottom=0.05,top=0.92,wspace=0.25,hspace=0.5)
+plt.subplots_adjust(left=0.08,right=0.92,bottom=0.05,top=0.92,wspace=0.25,hspace=0.1)
 
 for iut in range(nut):
     
-     
-    
     ut_in=ut[iut]
     
-    if ut_in<0:
-        continue
+    if output_type == '2dall':        
+        if ut_in<0:
+            continue
+        time1=time_ref+dt.timedelta(seconds=round(ut_in*3600))
     
+    if output_type == '3dall':
+        ut_in_dt = ut_in.astype('datetime64[s]').astype(dt.datetime)
+        delta_seconds = (ut_in_dt - time_ref).total_seconds()
+        time1 = time_ref + dt.timedelta(seconds=round(delta_seconds))
+
     
-    time1=time_ref+dt.timedelta(seconds=round(ut_in*3600))
     print (time1)
     
     time_str=time1.strftime("%Y%m%d_%H%MUT")
@@ -106,23 +111,24 @@ for iut in range(nut):
     ang=lon/180*np.pi                                                           
     r=90-lat
     
+    
     ang,r=np.meshgrid(ang,r)                                                    
     ang=ang.T                                                                   
     r=r.T                                                                       
                                                                                 
-    ang_offset=ut_in/12*np.pi-np.pi/2
+    ang_offset=int(ut_in)/12*np.pi-np.pi/2
     im=gitm_contourf(ax,parm,vmin,vmax,cs_step,cmap,ang,r,ang_offset)
 
     # Change the parameters if needed
     set_polar_plot(fig,ax,                                                      
                    50,32,'k','k',                                               
-                   time_str,0.04,32,'magenta',                                            
+                   time_str,0.04,48,'magenta',                                            
                    32,5,32,8,0) 
     
     
     
-save_fn = save_pt+by[:-1]+'.jpg'
+save_fn = save_pt+by[:-1]+'.pdf'
 print(save_fn)
-plt.savefig(save_fn) # Change it
+plt.savefig(save_fn, bbox_inches='tight', pad_inches=0) # Change it
 #plt.show()
 #plt.close()
